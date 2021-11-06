b27627fa3d6a51580e6fe824effe346b80d51724
Romain Pouclet <romain.pouclet@gmail.com>
2021-11-05 19:37:57 -0700

    Wrap unexpected errors from GitHub

--- Sources/Agamotto/DependencyCheck.swift
+++ Sources/Agamotto/DependencyCheck.swift
@@ -3,6 +3,7 @@ import GitHubClient
 
 public enum DependencyErrorType: Error {
     case unsupportedScm
+    case scmAPIError(Error)
 }
 public enum DependencyCheckResult {
     case upToDate
@@ -17,24 +18,43 @@ public func checkDependency(dependency: Dependency) async throws -> DependencyCh
         return .error(type: .unsupportedScm)
     }
 
-    let latestRelease = try await client.getLatestRelease(repo: dependency.cloneURL.repoName)
+    do {
+        let latestRelease = try await client.getLatestRelease(repo: dependency.cloneURL.repoName)
 
-    if latestRelease.tagName != dependency.version {
-        return .outdated(currentVersion: dependency.version, latestVersion: latestRelease.tagName)
-    } else {
-        return .upToDate
+        if latestRelease.tagName != dependency.version {
+            return .outdated(currentVersion: dependency.version, latestVersion: latestRelease.tagName)
+        } else {
+            return .upToDate
+        }
+    } catch let error {
+        return .error(type: .scmAPIError(error))
     }
 }
 
 public func checkDependencies(packagePath: String) async throws {
     let deps = try parsePackage(path: packagePath)
 
-    await withThrowingTaskGroup(of: DependencyCheckResult.self) { group in
+    let statuses = try await withThrowingTaskGroup(of: (Dependency, DependencyCheckResult).self, returning: [(Dependency, DependencyCheckResult)].self) { group in
+        var statuses = [(Dependency, DependencyCheckResult)]()
+        statuses.reserveCapacity(deps.count)
+
         for dep in deps {
             group.addTask(priority: .medium) {
-                try await checkDependency(dependency: dep)
+                try (dep, await checkDependency(dependency: dep))
             }
         }
+
+        return try await group.reduce(into: statuses) { $0.append($1) }
     }
 
+    for (dep, status) in statuses {
+        switch status {
+        case .upToDate:
+            print("[\(dep.name)] Dependency is up to date")
+        case .outdated(currentVersion: let currentVersion, latestVersion: let latestVersion):
+            print("[\(dep.name)] Dependency should be updated from \(currentVersion) to \(latestVersion)")
+        case .error(type: let type):
+            print("[\(dep.name)] There was an error checking for that dependency: \(type)")
+        }
+    }
 }
