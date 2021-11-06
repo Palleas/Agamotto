d9635b7d49e9539aa63b33c4f3eb739e5dcbca8e
Romain Pouclet <romain.pouclet@gmail.com>
2021-11-05 18:49:50 -0700

    Add new types fpr CloneUrl and RepoName

--- Sources/Agamotto/DependencyCheck.swift
+++ Sources/Agamotto/DependencyCheck.swift
@@ -1,28 +1,40 @@
 import Foundation
 import GitHubClient
 
+public enum DependencyErrorType: Error {
+    case unsupportedScm
+}
 public enum DependencyCheckResult {
     case upToDate
     case outdated(currentVersion: String, latestVersion: String)
-    case error(message: String)
+    case error(type: DependencyErrorType)
 }
 
 let client = GithubClient()
 
 public func checkDependency(dependency: Dependency) async throws -> DependencyCheckResult {
-    guard dependency.url.host == "github.com" else {
-        return .error(message: "Only GitHub.com is supported")
+    guard try dependency.cloneURL.host == "github.com" else {
+        return .error(type: .unsupportedScm)
     }
 
-    let pieces = dependency.url.path.split(separator: "/", maxSplits: 2, omittingEmptySubsequences: true)
-    let owner = String(pieces[0])
-    let name = String(pieces[1]).replacingOccurrences(of: ".git", with: "")
-
-    let latestRelease = try await client.getLatestRelease(repo: RepoName(owner: owner, name: name))
+    let latestRelease = try await client.getLatestRelease(repo: dependency.cloneURL.repoName)
 
     if latestRelease.tagName != dependency.version {
         return .outdated(currentVersion: dependency.version, latestVersion: latestRelease.tagName)
     } else {
         return .upToDate
     }
+}
+
+public func checkDependencies(packagePath: String) async throws {
+    let deps = try parsePackage(path: packagePath)
+
+    await withThrowingTaskGroup(of: DependencyCheckResult.self) { group in
+        for dep in deps {
+            group.addTask(priority: .medium) {
+                try await checkDependency(dependency: dep)
+            }
+        }
+    }
+
 }
