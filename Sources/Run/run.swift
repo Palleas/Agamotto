d58d5d5d6339198d672a0526ffd478790b72cfbb
Romain Pouclet <romain.pouclet@gmail.com>
2021-11-13 21:32:53 -0800

    Setup argument parser

--- Sources/Run/run.swift
+++ Sources/Run/run.swift
@@ -1,14 +1,24 @@
 import Agamotto
 import Foundation
+import ArgumentParser
 
 @main
-struct Main {
-    static func main() async throws {
-        guard let path = ProcessInfo.processInfo.arguments.dropFirst().first else {
-            print("Missing path to folder containing Package.swift")
-            exit(1)
-        }
+struct CheckDependenciesCommand: ParsableCommand {
 
-        try await checkDependencies(packagePath: path)
+    @Argument(completion: .directory)
+    private var packagePath: String
+
+    func run() async throws {
+        print("Package: \(packagePath)")
+        try await checkDependencies(packagePath: packagePath)
     }
+//    static func main() async throws {
+//        guard let path = ProcessInfo.processInfo.arguments.dropFirst().first else {
+//            print("Missing path to folder containing Package.swift")
+////            exit(1)
+//            return
+//        }
+//
+//        try await checkDependencies(packagePath: path)
+//    }
 }
