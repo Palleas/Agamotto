f0e1593d01a79805fe44daa212751636771a21d7
Romain Pouclet <romain.pouclet@gmail.com>
2023-12-30 08:40:45 +0100

    Add version

--- Sources/Run/entrypoint.swift
+++ Sources/Run/entrypoint.swift
@@ -7,6 +7,7 @@ struct Entrypoint: AsyncParsableCommand {
 
     static let configuration = CommandConfiguration(
         commandName: "agamotto",
+        version: "0.1.0",
         subcommands: [CheckDependenciesCommand.self],
         defaultSubcommand: CheckDependenciesCommand.self
     )
