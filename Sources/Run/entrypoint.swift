b14de6378fee9485229306c931149e3df9a098f2
Romain Pouclet <code@romain-pouclet.com>
2024-07-15 10:35:35 -0700

    Bump to 0.2.0

--- Sources/Run/entrypoint.swift
+++ Sources/Run/entrypoint.swift
@@ -7,7 +7,7 @@ struct Entrypoint: AsyncParsableCommand {
 
     static let configuration = CommandConfiguration(
         commandName: "agamotto",
-        version: "0.1.0",
+        version: "0.2.0",
         subcommands: [CheckDependenciesCommand.self],
         defaultSubcommand: CheckDependenciesCommand.self
     )
